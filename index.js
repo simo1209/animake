9d0971b05af1d0c235811f1ca7d8eb8df43152cf
Simeon Georgiev <simeongeorgiev09@gmail.com>
2020-06-22 18:02:38 +0300

    Added user list system

--- index.js
+++ index.js
@@ -5,32 +5,51 @@ let io = require('socket.io')(http);
 
 app.use(express.static('public'))
 
-let word = "cat";
+let words = ["dog", "cat", "pikachu", "stickamn"];
+
+let rooms = {};
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     let room;
     let nick;
 
-    socket.on('room',(r)=>{
-        socket.join(r);
-        room = r;
-    })
-    socket.on('nick',(n)=>{
+    socket.on('nick', (n) => { // When the client emits it's nickname
         nick = n;
+        if (rooms[room]) { // If there are already players in the room
+            rooms[room].push({ nickname: n }); // add the current nickname to the room
+        } else {
+            rooms[room] = [{ nickname: n }]; // or create new room and add the current nick
+        }
+        io.to(room).emit("joined", rooms[room]); // emit the current room to everyone in it
     })
 
-    socket.on('line', (msg) => {
-        socket.broadcast.to(room).emit("draw",msg);
+    socket.on('room', (r) => { // when someone tries to enter a room
+        socket.join(r); // add him to the room
+        room = r; // and set the socket pair room
+    })
+
+    socket.on('line', (line) => { // When player draws line
+        socket.broadcast.to(room).emit("draw", line); // Draw it on everyone else's canvas
     });
 
-    socket.on('chat', (msg)=>{
-        console.log(nick,msg)
-        io.to(room).emit('chat',(nick+": "+msg));
+    socket.on('chat', (msg) => { // When someone sends message to chat 
+        io.to(room).emit('chat', { nikname: nick, message: msg }); // Broadcast it to the room
     })
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        if (rooms[room]) {
+            let index;
+            for(let i =0; i<rooms[room].length;++i){
+                if(rooms[room][i].nickname == nick){
+                    index
+                }
+            };
+            if (index !== -1) rooms[room].splice(index, 1);
+            io.to(room).emit("joined", rooms[room]); // emit the current room to everyone in it
+            console.log(rooms);
+        }
     });
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,37 @@
 let socket = io();
 
-let room = window.location.search.split('?')[1];
-if (room) {
-    socket.emit('room', room);
-    console.log("room:", room);
-}
+let room;
+
+$(function () {
+    $('#drawing').hide();
+
+    $('#textbox>form').submit(function (e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    $('#nick-form').submit(function (e) {
+
+        room = window.location.search.split('?')[1];
+        if (room) {
+            socket.emit('room', room);
+            console.log("room:", room);
+        }
+
+        e.preventDefault(); // prevents page reloading
+        // socket.emit('chat', $('#m').val());
+        console.log("sending nick:", $('#nick').val());
+        socket.emit('nick', $('#nick').val());
+        $('#nickname').hide();
+        $('#drawing').show();
+        return false;
+    });
+});
 
 
-socket.on("chat",(msg)=>{
+socket.on("chat", (msg) => {
     let node = createP(msg);
     console.log(msg);
     select("#messages").child(node);
@@ -33,25 +57,12 @@ socket.on("draw", (msg) => {
     // console.log(msg);
     stroke(200, 0, 100);
     line(msg.pmouseX, msg.pmouseY, msg.mouseX, msg.mouseY);
-})  
+})
 
-$(function () {
-    $('#drawing').hide();
-
-    $('#textbox>form').submit(function(e) {
-      e.preventDefault(); // prevents page reloading
-      socket.emit('chat', $('#m').val());
-      $('#m').val('');
-      return false;
+socket.on("joined", (players) => {
+    console.log(players);
+    select("#players").html("");
+    players.forEach(player => {
+        select("#players").child(createDiv(player.nickname));
     });
-
-    $('#nick-form').submit(function(e) {
-        e.preventDefault(); // prevents page reloading
-        // socket.emit('chat', $('#m').val());
-        console.log("sending nick:",$('#nick').val());
-        socket.emit('nick', $('#nick').val());
-        $('#nickname').hide();
-        $('#drawing').show();
-        return false;
-      });
-  });
+})
